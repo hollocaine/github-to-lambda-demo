a410416ec5171a9d30e4f4040bd046ee209d147f
hollocaine <alanflynnn@gmail.com>
2022-03-13 10:35:38 +0000

    Added lambda and requirements

--- lambda_function.py
+++ lambda_function.py
@@ -2,5 +2,6 @@ import pandas as pd
 def lambda_handler(event, context):
     d = {"col1": [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
+    print('some changes')
     print(df)
     print('Done processing')
